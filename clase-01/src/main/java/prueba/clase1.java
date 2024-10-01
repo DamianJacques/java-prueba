88a31f5b28b43d230d2838174fae7d628c257337
DamianJacques <damianjacques.79@gmail.com>
2024-10-01 18:45:39 -0300

    segundo commit

--- clase-01/src/main/java/prueba/clase1.java
+++ clase-01/src/main/java/prueba/clase1.java
@@ -4,8 +4,8 @@ public class clase1 {
 	public static void main (String[] args) {
 float num1=5;
 float num2=5;
-float resul;
-resul=num1+num2;
-System.out.println(resul);
+float resultado;
+resultado=num1+num2;
+System.out.println(resultado);
 	}
 }
