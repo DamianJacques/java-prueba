0a8ba7aa439881bbefad4de5481113b32c6f0b21
DamianJacques <damianjacques.79@gmail.com>
2024-10-01 18:43:49 -0300

    primer commit eclipse programando

--- clase-01/src/main/java/prueba/clase1.java
+++ clase-01/src/main/java/prueba/clase1.java
@@ -1,5 +1,11 @@
 package prueba;
 
 public class clase1 {
-
+	public static void main (String[] args) {
+float num1=5;
+float num2=5;
+float resul;
+resul=num1+num2;
+System.out.println(resul);
+	}
 }
